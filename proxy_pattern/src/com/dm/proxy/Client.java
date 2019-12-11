26aacaa571f865ac594b2e110b99a5815732b533
dm13579 <19031903959864@qq.com>
2019-12-11 11:28:05 +0800

    代理模式

---
+++ proxy_pattern/src/com/dm/proxy/Client.java
@@ -0,0 +1,15 @@
+package com.dm.proxy;
+
+/**
+ * @author Administrator
+ * @version V1.0
+ * @ClassName:client
+ * @Description:TODO
+ * @date 2019/12/10
+ */
+public class Client {
+    public static void main(String[] args) {
+        Proxy proxy = new Proxy();
+        proxy.request();
+    }
+}
